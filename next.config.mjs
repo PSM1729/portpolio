6d369bf55d9e4b2b672724e2cc0573c9b77ee5b5
psm1729 <sabitamishra1729@gmail.com>
2025-09-10 21:39:23 +0530

    Change in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 const isProd = process.env.NODE_ENV === "production";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", 
+  distDir: "out",
   images: {
     unoptimized: true, 
   },
-  basePath: isProd ? "/portpolio" : "", 
+  basePath: isProd ? "/portpolio" : "",
   assetPrefix: isProd ? "/portpolio/" : "",
 };
 
